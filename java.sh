df9048ffc8808d67686ec53dad2e9ec5202ca0ed
Pannala Sai Kumar Reddy <114500505+SkrDevOps@users.noreply.github.com>
2022-10-06 18:03:11 +0530

    Update java.sh

--- java.sh
+++ java.sh
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
+
 "hello guys.. this is sai kumar reddy"
-<<<<<<< HEAD
-<<<<<<< HEAD
-"hello hustle"
-=======
 "this is the latest modification"
 "and this good decision taken by me"
 "last line in java"
-<<<<<<< HEAD
 "this is the new code in remote repository"
-"ok this is the end of the file"
-=======
-=======
 "hello hustle"
->>>>>>> 9140f0ebc0e40f891cacb48cbf093ff8b083e867
->>>>>>> development
-=======
 "hello guys.. this is sai kumar reddy"
 "this is the latest modification"
 "and this good decision taken by me"
@@ -23,4 +12,3 @@
 "this is the new code in remote repository"
 "ok this is the end of the file"
 
->>>>>>> be76233780128e36677f0827ae3999dc1dad67a9
